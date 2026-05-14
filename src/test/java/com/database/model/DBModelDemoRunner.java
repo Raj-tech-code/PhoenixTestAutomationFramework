c89c05c112abcf2b2893d7cbb46662ffab9eb920
Rajasekar K <rajasekar.k0703@gmail.com>
2026-05-14 19:12:32 +0530

    Added second DB validation scenario for response model deserialization

--- src/test/java/com/database/model/DBModelDemoRunner.java
+++ src/test/java/com/database/model/DBModelDemoRunner.java
@@ -5,10 +5,7 @@ public class DBModelDemoRunner {
 
 
 	public static void main(String[] args) {
-		
-//		CustomerDBModel customerDBModel = new CustomerDBModel("Rajasekar", "K", "9025394089", "9045665566", "raj@gmail.com", "Raj@gmail.com");
 
-//		System.out.println(customerDBModel);
 	}
 			
 			
